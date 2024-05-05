f6433639c7d2f3bebbf5b9ec7015878fa78d072d
Abhay Tomar <apstomar456@gmail.com>
2024-05-05 17:07:42 +0530

    crowdfunding project complete

---
+++ Components/index.js
@@ -0,0 +1,8 @@
+import Footer from "./Footer";
+import NavBar from "./NavBar";
+import Hero from "./Hero";
+import PupUp from "./PupUp";
+import Card from "./Card";
+import Menu from "./menu";
+
+export { NavBar, Footer, Hero, Card, PupUp, Menu };
